bd34d13db78e821857f38cdd4c45e8daa7a980eb
Karan Dalvi <karan.j.dalvi@gmail.com>
2017-09-29 01:12:10 -0400

    expression test cases

--- src/cop5556fa17/SimpleParserTest.java
+++ src/cop5556fa17/SimpleParserTest.java
@@ -98,7 +98,8 @@ public class SimpleParserTest {
 	
 	@Test
 	public void variableDeclaration01() throws LexicalException, SyntaxException {
-		String[] valid = new String[] {"int ident1;"};
+		//TODO Insert int ident = (valid expressions)
+		String[] valid = new String[] {"int ident1", "boolean b1", "int indent1 = 100", "int ident1 = 2 + 2"};
 		for (String input : valid) {
 			Scanner scanner = new Scanner(input).scan();  //Create a Scanner and initialize it
 			SimpleParser parser = new SimpleParser(scanner);  //Create a parser
@@ -111,21 +112,9 @@ public class SimpleParserTest {
 			}
 		}
 	}
+	
 	@Test
 	public void variableDeclaration02() throws LexicalException, SyntaxException {
-		String input = "boolean ident1";  //The input is the empty string.  This is not legal
-		Scanner scanner = new Scanner(input).scan();  //Create a Scanner and initialize it
-		SimpleParser parser = new SimpleParser(scanner);  //Create a parser
-		try {
-			parser.variableDeclaration();  //Parse the program
-		}
-		catch (SyntaxException e) {
-			show(e);
-			throw e;
-		}
-	}
-	@Test
-	public void variableDeclaration03() throws LexicalException, SyntaxException {
 		String input = "int int";  //The input is the empty string.  This is not legal
 		Scanner scanner = new Scanner(input).scan();  //Create a Scanner and initialize it
 		SimpleParser parser = new SimpleParser(scanner);  //Create a parser
@@ -138,8 +127,9 @@ public class SimpleParserTest {
 			throw e;
 		}
 	}
+	
 	@Test
-	public void variableDeclaration04() throws LexicalException, SyntaxException {
+	public void variableDeclaration03() throws LexicalException, SyntaxException {
 		String input = "boolean x";  //The input is the empty string.  This is not legal
 		Scanner scanner = new Scanner(input).scan();  //Create a Scanner and initialize it
 		SimpleParser parser = new SimpleParser(scanner);  //Create a parser
@@ -154,10 +144,11 @@ public class SimpleParserTest {
 	}
 	
 	@Test
-	public void variableDeclaration05() throws LexicalException, SyntaxException {
-		String input = "int ident1 = 5";  //The input is the empty string.  This is not legal
+	public void variableDeclaration04() throws LexicalException, SyntaxException {
+		String input = "int ident1=;";  //The input is the empty string.  This is not legal
 		Scanner scanner = new Scanner(input).scan();  //Create a Scanner and initialize it
 		SimpleParser parser = new SimpleParser(scanner);  //Create a parser
+		thrown.expect(SyntaxException.class);
 		try {
 			parser.variableDeclaration();  //Parse the program
 		}
@@ -340,7 +331,142 @@ public class SimpleParserTest {
 		}
 	}
 	
+	@Test
+	public void sourceSinkDeclaration01() throws LexicalException, SyntaxException {
+		String[] valid = new String[] {"url ident1 = ident2", "file ident1 = ident2", 
+		"url indent1 = @ 100", "url ident1 = \"hello\""};
+		for (String input : valid) {
+			Scanner scanner = new Scanner(input).scan();  //Create a Scanner and initialize it
+			SimpleParser parser = new SimpleParser(scanner);  //Create a parser
+			try {
+				parser.sourceSinkDeclaration();  //Parse the program
+			}
+			catch (SyntaxException e) {
+				show(e);
+				throw e;
+			}
+		}
+	}
 	
+	@Test
+	public void sourceSinkDeclaration02() throws LexicalException, SyntaxException {
+		String input = "keyword url";
+		Scanner scanner = new Scanner(input).scan();  //Create a Scanner and initialize it
+		SimpleParser parser = new SimpleParser(scanner);  //Create a parser
+		thrown.expect(SyntaxException.class);
+		try {
+			parser.sourceSinkDeclaration();  //Parse the program
+		}
+		catch (SyntaxException e) {
+			show(e);
+			throw e;
+		}
+	}
+
+	@Test
+	public void sourceSinkDeclaration03() throws LexicalException, SyntaxException {
+		String input = "url ident = ;";
+		Scanner scanner = new Scanner(input).scan();  //Create a Scanner and initialize it
+		SimpleParser parser = new SimpleParser(scanner);  //Create a parser
+		thrown.expect(SyntaxException.class);
+		try {
+			parser.sourceSinkDeclaration();  //Parse the program
+		}
+		catch (SyntaxException e) {
+			show(e);
+			throw e;
+		}
+	}
+	
+	@Test
+	public void sourceSinkDeclaration04() throws LexicalException, SyntaxException {
+		String input = "file helloWorld";
+		Scanner scanner = new Scanner(input).scan();  //Create a Scanner and initialize it
+		SimpleParser parser = new SimpleParser(scanner);  //Create a parser
+		thrown.expect(SyntaxException.class);
+		try {
+			parser.sourceSinkDeclaration();  //Parse the program
+		}
+		catch (SyntaxException e) {
+			show(e);
+			throw e;
+		}
+	}
+	
+	@Test
+	public void sourceSinkDeclaration05() throws LexicalException, SyntaxException {
+		String input = "file ";
+		Scanner scanner = new Scanner(input).scan();  //Create a Scanner and initialize it
+		SimpleParser parser = new SimpleParser(scanner);  //Create a parser
+		thrown.expect(SyntaxException.class);
+		try {
+			parser.sourceSinkDeclaration();  //Parse the program
+		}
+		catch (SyntaxException e) {
+			show(e);
+			throw e;
+		}
+	}
+	
+	@Test
+	public void expression01() throws LexicalException, SyntaxException {
+		String[] valid = new String[] {
+				"25", "(0)", "sin(90)", "cos(45)", "atan(0)", "abs(45)",
+				"cart_x(100)", "cart_y(100)", "polar_a(10)", "polar_r(10)",
+				"(sin(90))", "(cos(45))", "(atan(0))", "(abs(45))",
+				"(cart_x(100))", "(cart_y(100))", "(polar_a(10))", "(polar_r(10))",
+				"sin[10, 20]", "cos[sin(90), 20]", "atan[(10), (abs(45))]",
+				"ident [10,20]", "ident [sin(90), cos(90)]", "ident", "x", "y", "r", "a",
+				"X", "Y", "Z", "A", "R", "DEF_X", "DEF_Y",	
+				"!25", "!(0)", "!sin(90)", "!cos(45)", "!atan(0)", "!abs(45)",
+				"!cart_x(100)", "!cart_y(100)", "!polar_a(10)", "!polar_r(10)",
+				"!(sin(90))", "!(cos(45))", "!(atan(0))", "!(abs(45))",
+				"!(cart_x(100))", "!(cart_y(100))", "!(polar_a(10))", "!(polar_r(10))",
+				"!sin[10, 20]", "!cos[sin(90), 20]", "!atan[(10), (abs(45))]",
+				"!ident [10,20]", "!ident [sin(90), cos(90)]", "!ident", "!x", "!y", "!r", "!a",
+				"!X", "!Y", "!Z", "!A", "!R", "!DEF_X", "!DEF_Y",
+				"+25", "+(0)", "+sin(90)", "+cos(45)", "+atan(0)", "+abs(45)",
+				"+cart_x(100)", "+cart_y(100)", "+polar_a(10)", "+polar_r(10)",
+				"+(sin(90))", "+(cos(45))", "+(atan(0))", "+(abs(45))",
+				"+(cart_x(100))", "+(cart_y(100))", "+(polar_a(10))", "+(polar_r(10))",
+				"+sin[10, 20]", "+cos[sin(90), 20]", "+atan[(10), (abs(45))]",
+				"+ident [10,20]", "+ident [sin(90), cos(90)]", "+ident", "+x", "+y", "+r", "+a",
+				"+X", "+Y", "+Z", "+A", "+R", "+DEF_X", "+DEF_Y",
+				"-25", "-(0)", "-sin(90)", "-cos(45)", "-atan(0)", "-abs(45)",
+				"-cart_x(100)", "-cart_y(100)", "-polar_a(10)", "-polar_r(10)",
+				"-(sin(90))", "-(cos(45))", "-(atan(0))", "-(abs(45))",
+				"-(cart_x(100))", "-(cart_y(100))", "-(polar_a(10))", "-(polar_r(10))",
+				"-sin[10, 20]", "-cos[sin(90), 20]", "-atan[(10), (abs(45))]",
+				"-ident [10,20]", "-ident [sin(90), cos(90)]", "-ident", "-x", "-y", "-r", "-a",
+				"-X", "-Y", "-Z", "-A", "-R", "-DEF_X", "-DEF_Y",
+				"+++---+25", "+++---+(0)", "+++---+sin(90)", "+++---+cos(45)", "+++---+atan(0)", "+++---+abs(45)",
+				"+++---+cart_x(100)", "+++---+cart_y(100)", "+++---+polar_a(10)", "+++---+polar_r(10)",
+				"+++---+(sin(90))", "+++---+(cos(45))", "+++---+(atan(0))", "+++---+(abs(45))",
+				"+++---+(cart_x(100))", "+++---+(cart_y(100))", "+++---+(polar_a(10))", "+++---+(polar_r(10))",
+				"+++---+sin[10, 20]", "+++---+cos[sin(90), 20]", "+++---+atan[(10), (abs(45))]",
+				"+++---+ident [10,20]", "+++---+ident [sin(90), cos(90)]", "+++---+ident", "+++---+x", "+++---+y", "+++---+r", "+++---+a",
+				"+++---+X", "+++---+Y", "+++---+Z", "+++---+A", "+++---+R", "+++---+DEF_X", "+++---+DEF_Y",
+				"sin(90)*++25", "sin(90)*++(0)", "sin(90)*++sin(90)", "--sin(90)*++cos(45)", "--sin(90)*++atan(0)", "--sin(90)*++abs(45)",
+				"10*++cart_x(100)", "10*++cart_y(100)", "10*++polar_a(10)", "10*++polar_r(10)",
+				"(50)*(sin(90))", "+++---+(cos(45))", "+++---+(atan(0))", "+++---+(abs(45))",
+				"+++---+(cart_x(100))", "-x*(cart_y(100))", "+++---+(polar_a(10))", "+++---+(polar_r(10))",
+				"+++---+sin[10, 20]", "+++---+cos[sin(90), 20]", "+++---+atan[(10), (abs(45))]",
+				"+++---+ident [10,20]", "+++---+ident [sin(90), cos(90)]", "ident [sin(90), cos(90)]%ident", "ident [sin(90), cos(90)]%x", "DEF_Y%y", "DEF_Y%r", "DEF_Y%a",
+				"identif%X", "--identif%Y", "--identif%Z", "((((ident))))%A", "((((+ident))))%R", "-((((+ident))))%DEF_X", "-((((+10))))%DEF_Y",
+				"++sin(90)++sin(90)","+x-+y", "+x-+y > sin(10)", "true", "false", "+true", "-false"};
+		for (String input : valid) {
+			Scanner scanner = new Scanner(input).scan();  //Create a Scanner and initialize it
+			SimpleParser parser = new SimpleParser(scanner);  //Create a parser
+			try {
+				parser.expression();  
+			}
+			catch (SyntaxException e) {
+				System.out.println("Error parsing " + input);
+				show(e);
+				throw e;
+			}
+		}
+	}
 	//---------------------------------------------------------------------
 	
 	/** Another example.  This is a legal program and should pass when 
